e378ec4a2574545a300647210a3c3abaa4cd1b64
Mikhail Ushanov <gm.mephisto@gmail.com>
2017-06-05 17:53:57 +0300

    cases: added security group rules
    Signed-off-by: Mikhail Ushanov <gm.mephisto@gmail.com>

---
+++ cases/security_group_rule_all/main.tf
@@ -0,0 +1,11 @@
+variable subnet_id {}
+
+resource "aws_security_group_rule" "allow_all" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${var.subnet_id}"
+}
