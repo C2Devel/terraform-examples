3fe9d08acfa1b0681ebc597b477256de1daf53d6
Mikhail Ushanov <gm.mephisto@gmail.com>
2017-06-06 14:10:33 +0300

    cases: added note about vpn gateway id
    Signed-off-by: Mikhail Ushanov <gm.mephisto@gmail.com>

--- cases/customer_gateways_and_vpn/main.tf
+++ cases/customer_gateways_and_vpn/main.tf
@@ -9,7 +9,10 @@ resource "aws_customer_gateway" "test_customer_gateway" {
 }
 
 resource "aws_vpn_connection" "test_vpn" {
-  vpn_gateway_id      = "${data.aws_vpc.test_vpc.id}"
+  # NOTE: VPN Gateway entity implemented inside VPC entity,
+  #       so use VPC Id as VPN Gateway Id
+  vpn_gateway_id = "${data.aws_vpc.test_vpc.id}"
+
   customer_gateway_id = "${aws_customer_gateway.test_customer_gateway.id}"
   type                = "ipsec.1"
 }
