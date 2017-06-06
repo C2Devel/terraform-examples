6de1c4ed5d7d63ca13d9ecb3a09b5670f28f2bc3
Mikhail Ushanov <gm.mephisto@gmail.com>
2017-06-06 14:05:44 +0300

    provider: update comment linting
    Signed-off-by: Mikhail Ushanov <gm.mephisto@gmail.com>

--- main.tf
+++ main.tf
@@ -21,10 +21,12 @@ provider "aws" {
 
   # NOTE: STS API is not implemented, skip validation
   skip_credentials_validation = true
+
   # NOTE: IAM API is not implemented, skip validation
-  skip_requesting_account_id  = true
+  skip_requesting_account_id = true
+
   # NOTE: Region has different name, skip validation
-  skip_region_validation      = true
+  skip_region_validation = true
 
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
