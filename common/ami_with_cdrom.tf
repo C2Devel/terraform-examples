17e884e80c7d5939125863627c3f63768fc21a89
Mikhail Ushanov <gm.mephisto@gmail.com>
2018-01-31 13:34:58 +0300

    common: added ami with cdrom in blockdevicemapping
    Signed-off-by: Mikhail Ushanov <gm.mephisto@gmail.com>

---
+++ common/ami_with_cdrom.tf
@@ -0,0 +1,31 @@
+resource "aws_ebs_volume" "test_volume" {
+  availability_zone = "${var.az}"
+  size              = 10
+}
+
+resource "aws_ebs_snapshot" "test_snapshot" {
+  volume_id = "${aws_ebs_volume.test_volume.id}"
+}
+
+resource "aws_ami" "test_ami_with_cdrom" {
+  # NOTE: 'name' attribute is not supported, leave blank
+  name = ""
+
+  # NOTE: 'virtualization_type' attribute must be overrided
+  #       with 'kvm-virtio' or 'kvm-legacy' value
+  virtualization_type = "kvm-virtio"
+
+  # NOTE: 'sriov_net_support' attribute is not supported, leave blank
+  sriov_net_support = ""
+  root_device_name  = "disk1"
+
+  ephemeral_block_device {
+    device_name  = "cdrom1"
+    virtual_name = "cdrom1"
+  }
+
+  ebs_block_device {
+    device_name = "disk1"
+    snapshot_id = "${aws_ebs_snapshot.test_snapshot.id}"
+  }
+}
