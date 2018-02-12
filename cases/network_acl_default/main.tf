0e5cd31a419da3ebae3f08df40ece5c162dad319
Pavel Ezhov <pezhov@croc.ru>
2018-02-12 16:34:50 +0300

    cases: add modify default network ACL case

---
+++ cases/network_acl_default/main.tf
@@ -0,0 +1,21 @@
+resource "aws_default_network_acl" "default_network_acl" {
+  default_network_acl_id = "${aws_vpc.test_vpc.default_network_acl_id}"
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
