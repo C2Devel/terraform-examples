844efc412939bcb221c5d016c817d88a91c686b8
Nikita Kretov <nkretov@croc.ru>
2018-12-14 15:27:18 +0300

    aws_instance run_instance_with_ebs_override: mv case to aws_instance

--- cases/aws_instance/run_instance_with_ebs_override/main.tf
+++ cases/aws_instance/run_instance_with_ebs_override/main.tf
@@ -1,14 +1,12 @@
-variable "subnet_id" {}
-
 resource "aws_instance" "test_instance_with_override" {
-  ami           = "${aws_ami.test_ami.id}"
+  ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
-  subnet_id     = "${var.subnet_id}"
+  subnet_id     = "${aws_subnet.test_subnet.id}"
 
   # NOTE: 'root_block_device' section is required for
   #       proper detection of the instance root device
   root_block_device {
-    volume_size           = 15
+    volume_size           = 32
     delete_on_termination = false
   }
 
@@ -16,6 +14,13 @@ resource "aws_instance" "test_instance_with_override" {
   #       defined in 'ebs_block_device' section
   ebs_block_device {
     device_name = "disk2"
-    volume_size = 5
+    volume_size = 32
+  }
+
+  ebs_block_device {
+    device_name = "disk3"
+    volume_size = 15
+    volume_type = "io1"
+    iops        = 1000
   }
 }
