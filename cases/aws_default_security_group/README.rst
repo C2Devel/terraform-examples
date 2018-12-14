43ae9dd8e1a465a34bd4678f8071ef10e132b04c
Nikita Kretov <nkretov@croc.ru>
2018-12-14 15:22:04 +0300

    aws_default_security_group: security_group_default -> aws_default_security_group

---
+++ cases/aws_default_security_group/README.rst
@@ -0,0 +1,19 @@
+aws_default_security_group
+==========================
+
+Summary
+-------
+This example introduces ``aws_default_security_group`` resource.
+
+Differences
+-----------
+
+Notes
+~~~~~
+
+For more information visit :doc:`aws_security_group <../aws_security_group/README>`.
+
+
+Example
+-------
+.. literalinclude:: main.tf
--- cases/aws_default_security_group/main.tf
+++ cases/aws_default_security_group/main.tf
@@ -1,4 +1,5 @@
 resource "aws_default_security_group" "default_security_group" {
+  # NOTE: for more information check 'aws_security_group' case
   vpc_id = "${aws_vpc.test_vpc.id}"
 
   ingress {
@@ -9,9 +10,9 @@ resource "aws_default_security_group" "default_security_group" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
   }
 }
