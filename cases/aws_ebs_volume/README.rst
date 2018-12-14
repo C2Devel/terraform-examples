309f0169acc80ea82272d0653c53cb26a879f2df
Nikita Kretov <nkretov@croc.ru>
2018-12-14 15:19:09 +0300

    aws_ebs_volume: add new case

---
+++ cases/aws_ebs_volume/README.rst
@@ -0,0 +1,24 @@
+aws_ebs_volume
+==============
+
+Summary
+-------
+This example introduces ``aws_ebs_volume`` resource.
+
+Differences
+-----------
+
+Unsupported attributes
+~~~~~~~~~~~~~~~~~~~~~~
+
+* ``encrypted``
+* ``kms_key_id``
+
+Notes
+~~~~~
+
+``io1`` and ``st2`` are valid values for ``type`` attribute. ``iops = "400"`` is the only possible iops specification for disks with ``st2`` volume type. Disks with ``st2`` volume type must have ``size`` attribute value more then ``32G``.
+
+Example
+-------
+.. literalinclude:: main.tf
---
+++ cases/aws_ebs_volume/main.tf
@@ -0,0 +1,19 @@
+variable "types" {
+  type    = "list"
+  default = ["io1", "st2"]
+}
+
+resource "aws_ebs_volume" "test_volume_iops" {
+  # NOTE: 'encrypted', 'kms_key_id' attributes are not supported.
+  #       'io1' and 'st2' are valid values for 'type' attribute.
+  #       'iops = "400"' is only possible iops specification
+  #       for disks with 'st2' volume type. Disks with 'st2'
+  #       volume type must have size attribute value more then '32G'.
+  count = "${length(var.types)}"
+
+  availability_zone = "${var.az}"
+
+  size = 32
+  type = "${var.types[count.index]}"
+  iops = "400"
+}
