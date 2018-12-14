ec2acf1b5e2fa9524a2a6d6ef2a83287917b0783
Nikita Kretov <nkretov@croc.ru>
2018-12-14 15:43:30 +0300

    aws_security_group_rule: security_group_rule_cidr_blocks -> aws_security_group_rule

---
+++ cases/aws_security_group_rule/README.rst
@@ -0,0 +1,19 @@
+aws_security_group_rule
+=======================
+
+Summary
+-------
+This example introduces ``aws_security_group_rule`` resource.
+
+Differences
+-----------
+
+Unsupported attributes
+~~~~~~~~~~~~~~~~~~~~~~
+
+* ``prefix_list_ids``
+* ``description``
+
+Example
+-------
+.. literalinclude:: main.tf
---
+++ cases/aws_security_group_rule/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "source_security_group" {
+  vpc_id = "${aws_vpc.test_vpc.id}"
+  name   = "source_security_group"
+}
+
+resource "aws_security_group_rule" "allow_udp" {
+  # NOTE: 'prefix_list_ids ', 'description' attributes are not supported
+  type                     = "ingress"
+  from_port                = 11
+  to_port                  = 33
+  protocol                 = "udp"
+  source_security_group_id = "${aws_security_group.source_security_group.id}"
+  security_group_id        = "${aws_security_group.test_security_group.id}"
+}
+
+resource "aws_security_group_rule" "allow_icmp" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 255
+  protocol          = "icmp"
+  cidr_blocks       = ["1.1.1.0/24"]
+  security_group_id = "${aws_security_group.test_security_group.id}"
+}
