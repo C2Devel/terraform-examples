c07c206eeeecc0af1c818141ed092887397af033
Nikita Kretov <nkretov@croc.ru>
2018-12-18 23:41:42 +0300

    aws_ebs_snapshot: ebs_snapshot -> aws_ebs_snapshot

---
+++ cases/aws_ebs_snapshot/README.rst
@@ -0,0 +1,10 @@
+aws_ebs_snapshot
+================
+
+Summary
+-------
+This example introduces ``aws_ebs_snapshot`` resource.
+
+Example
+-------
+.. literalinclude:: main.tf
--- cases/ebs_snapshot/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ebs_snapshot" "test_snapshot_from_volume" {
-  volume_id = "${aws_ebs_volume.test_volume.id}"
-}
-
-resource "aws_ebs_volume" "test_volume_from_snapshot" {
-  availability_zone = "${var.az}"
-  snapshot_id       = "${aws_ebs_snapshot.test_snapshot_from_volume.id}"
-}
