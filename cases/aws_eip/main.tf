84316207f4ef72af7c4f828bee37a69a80ee431a
Nikita Kretov <kretov995@gmail.com>
2019-06-11 13:01:56 +0300

    [cases aws_eip] add aws_network_interface to aws_eip case

--- cases/aws_eip/main.tf
+++ cases/aws_eip/main.tf
@@ -1,27 +1,21 @@
-resource "aws_instance" "test_instance" {
+resource "aws_instance" "test1" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
   subnet_id     = "${aws_subnet.test_subnet.id}"
 }
 
-resource "aws_eip" "eip1" {
-  # NOTE: 'network_interface' and 'public_ipv4_pull' is not supported.
-  instance = "${aws_instance.test_instance.id}"
+resource "aws_eip" "test1" {
+  # NOTE: 'public_ipv4_pull' is not supported.
+  instance = "${aws_instance.test1.id}"
 
   vpc = true
 }
 
-resource "aws_instance" "test_instance2" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  subnet_id     = "${aws_subnet.test_subnet.id}"
-  private_ip    = "${cidrhost(aws_subnet.test_subnet.cidr_block, 100)}"
+resource "aws_network_interface" "test1" {
+  subnet_id = "${aws_subnet.test_subnet.id}"
 }
 
-resource "aws_eip" "eip2" {
-  # NOTE: 'network_interface' and 'public_ipv4_pull' is not supported.
+resource "aws_eip" "test2" {
   vpc = true
-
-  instance                  = "${aws_instance.test_instance2.id}"
-  associate_with_private_ip = "${aws_instance.test_instance2.private_ip}"
+  network_interface = "${aws_network_interface.test1.id}"
 }
