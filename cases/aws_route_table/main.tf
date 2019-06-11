b144e1f9181ba4105df829d209ff750cb8dce6f1
Nikita Kretov <kretov995@gmail.com>
2019-06-11 13:02:49 +0300

    [cases aws_route_table] add aws_network_interface to aws_route_table case

--- cases/aws_route_table/main.tf
+++ cases/aws_route_table/main.tf
@@ -3,7 +3,26 @@ resource "aws_route_table" "route_table" {
   vpc_id = "${aws_vpc.test_vpc.id}"
 
   route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = "${aws_vpc.test_vpc.id}"
+    # NOTE: 'ipv6_cidr_block', 'egress_only_gateway_id', 'gateway_id',
+    #       'nat_gateway_id', 'transit_gateway_id', 'vpc_peering_connection_id'
+    #       attributes are not supported for inline route object.
+    cidr_block = "10.0.2.0/24"
+    network_interface_id = "${aws_network_interface.test.id}"
   }
+
+  route {
+    cidr_block = "10.0.3.0/24"
+    instance_id = "${aws_instance.test.id}"
+  }
+
+}
+
+resource "aws_instance" "test" {
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id     = "${aws_subnet.test_subnet.id}"
+}
+
+resource "aws_network_interface" "test" {
+  subnet_id = "${aws_subnet.test_subnet.id}"
 }
