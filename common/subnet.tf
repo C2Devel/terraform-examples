9e83a44ba507734c1f9ecfc5687ab62459d66353
Nikita Kretov <kretov995@gmail.com>
2020-02-22 18:40:01 +0300

    common: update configs to 12.x

--- common/subnet.tf
+++ common/subnet.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "test_subnet" {
-  availability_zone = "${var.az}"
-  vpc_id = "${aws_vpc.test_vpc.id}"
+  availability_zone = var.az
+  vpc_id            = aws_vpc.test_vpc.id
 
-  cidr_block = "${cidrsubnet(aws_vpc.test_vpc.cidr_block, 1, 0)}"
+  cidr_block = cidrsubnet(aws_vpc.test_vpc.cidr_block, 1, 0)
 }
+
